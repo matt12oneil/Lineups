4227e44483a9982dc9936a5ff6667431c02c1030
matt12oneil <matt12oneil@gmail.com>
2023-03-21 11:49:21 -0400

    fixed with new baseballr college baseball functions

--- college_baseball_23.R
+++ college_baseball_23.R
@@ -19,7 +19,7 @@ library(tidytable)
 library(googlesheets4)
 devtools::install_github("BillPetti/baseballr")
 
-all_teams <- ncaa_teams(2023)
+#all_teams <- ncaa_teams(2023)
 # 
 # 
 # write.csv(all_teams, "/Users/mattoneil/Documents/MO/Lineups/college_teams", row.names=FALSE)
@@ -45,11 +45,11 @@ all_teams <- matt_teams %>%
 
 ncaa_schedule_info(team_id = 169, year = 2023)
 
-baseballr::load_ncaa_baseball_schedule()
+#baseballr::load_ncaa_baseball_schedule()
 
-team_name <- all_teams %>%
-  filter(team_id == 169) %>%
-  select(team_name)
+# team_name <- all_teams %>%
+#   filter(team_id == 169) %>%
+#   select(team_name)
 
 
 results <- function(team_id_number){
@@ -74,30 +74,27 @@ results <- function(team_id_number){
   all_teams <- matt_teams %>%
     bind_rows(ryan_teams)
   
-  baseballr::load_ncaa_baseball_schedule() %>%
-    filter(home_team_id %in% all_teams$team_id | away_team_id %in% all_teams$team_id) %>%
+  results <- baseballr::ncaa_schedule_info(team_id_number, year = 2023) %>%
     filter(!is.na(home_team_score) | !is.na(away_team_score)) %>%
-    filter(home_team_id == team_id_number | away_team_id == team_id_number) %>%
-    mutate(team = team_name$team_name)
+    mutate(team = team_name$team_name) %>%
+    mutate(winner = case_when(home_team_score > away_team_score ~ home_team,
+                              away_team_score > home_team_score ~ away_team,
+                              TRUE ~ 'Neither')) %>%
+    mutate(result = case_when(winner == team ~ 'W',
+                              TRUE ~ 'L'))
+  
+  return(results)
 
 }
 
+
+
 matt_results <- map_dfr(matt_teams$team_id, results) %>%
-  filter(!is.na(contest_id)) %>%
-  select(team, date, home_team, home_team_conference_id, home_team_score, away_team, away_team_score, away_team_conference_id) %>%
-  mutate(winner = case_when(home_team_score > away_team_score ~ home_team,
-                            home_team_score < away_team_score ~ away_team,
-                            TRUE ~ '')) %>%
-  mutate(result = case_when(team == winner ~ 'W',
-                            team != winner ~ 'L'))
+  #filter(!is.na(contest_id)) %>%
+  select(team, date, home_team, home_team_conference_id, home_team_score, away_team, away_team_score, away_team_conference_id, winner, result)
 ryan_results <- map_dfr(ryan_teams$team_id, results) %>%
-  filter(!is.na(contest_id)) %>%
-  select(team, date, home_team, home_team_conference_id, home_team_score, away_team, away_team_score, away_team_conference_id) %>%
-  mutate(winner = case_when(home_team_score > away_team_score ~ home_team,
-                            home_team_score < away_team_score ~ away_team,
-                            TRUE ~ '')) %>%
-  mutate(result = case_when(team == winner ~ 'W',
-                            team != winner ~ 'L'))
+  #filter(!is.na(contest_id)) %>%
+  select(team, date, home_team, home_team_conference_id, home_team_score, away_team, away_team_score, away_team_conference_id, winner, result)
 
 sheet_write(matt_results, ss = '1i2G5w_zqcuABeXnb0XY98pAsAUt361HwW6YkFuOSTrA', sheet = 'Matt Results')
 sheet_write(ryan_results, ss = '1i2G5w_zqcuABeXnb0XY98pAsAUt361HwW6YkFuOSTrA', sheet = 'Ryan Results')
