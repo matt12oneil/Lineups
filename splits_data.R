d5e1cc44fbecc71263a773bf5851beca911f919e
Matt O'Neil <matt12oneil@gmail.com>
2023-03-29 22:17:21 -0400

    Update splits_data.R

--- splits_data.R
+++ splits_data.R
@@ -51,12 +51,10 @@ pitchers <- events %>%
   tidytable::group_by(pitcher, p_throws, stand) %>%
   tidytable::summarize(events = n(), avg_launch_speed = mean(launch_speed, na.rm = T), avg_launch_angle = mean(launch_angle, na.rm = T), xba = mean(estimated_ba_using_speedangle, na.rm = T), woba = mean(woba_value, na.rm = T), xwoba = mean(estimated_woba_using_speedangle, na.rm = T), iso = mean(iso_value,na.rm = T), brl_pct = mean(barrel, na.rm = T))
 
-yy <- year(Sys.Date())
-mm <- month(Sys.Date())
-dd <- day(Sys.Date())
 
-readr::write_csv(batters,str_glue("splits_data/batter_splits_{yy}_{mm}_{dd}.csv"))
-readr::write_csv(pitchers,str_glue("splits_data/pitcher_splits_{yy}_{mm}_{dd}.csv"))
+
+readr::write_csv(batters,"splits_data/batter_splits.csv")
+readr::write_csv(pitchers,"splits_data/pitcher_splits.csv")
 
 
 
